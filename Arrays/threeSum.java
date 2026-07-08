217c6ccb386fbef527854a4c5cb9e12a49c36297
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-08 18:00:06 +0530

    Three sum

---
+++ Arrays/threeSum.java
@@ -0,0 +1,16 @@
+public class threeSum {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8};
+        int target = 21;
+        for(int i=0;i<arr.length-1;i++){
+           for(int j=i+1;j<arr.length;j++){
+            for(int k=j+1;k<arr.length;k++){
+            if(arr[i]+arr[j]+arr[k]==target){
+                System.err.print(arr[i]+" "+arr[j]+" "+arr[k]);
+            }
+        }
+           }
+            System.out.println();
+        }
+    }
+}
