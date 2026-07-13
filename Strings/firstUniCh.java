35be4ec4be527afa2a6b8b98d69e0bc6284419e9
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-13 18:43:30 +0530

    First unique ch

---
+++ Strings/firstUniCh.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class firstUniCh {
+    public static int printUni(String s){
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(map.get(ch)==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        String s = "kabeer";
+       System.out.println(printUni(s));
+    }
+}
