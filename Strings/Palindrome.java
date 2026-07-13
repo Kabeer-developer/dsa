529c3e89394774c8c67caa3afc0aa0c21f4788c6
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-13 18:43:47 +0530

    Palindrome

---
+++ Strings/Palindrome.java
@@ -0,0 +1,18 @@
+public class Palindrome {
+    public static boolean checkPalindrome(String str){
+        boolean isPalindrome = true;
+        for(int i=0;i<str.length()/2;i++){
+            if(str.charAt(i) != str.charAt(str.length()-i-1)){
+                isPalindrome=false;
+            }
+        }
+        return isPalindrome;
+    }
+    public static void main(String[] args) {
+        String str = "helle";
+        String str2 = "madam";
+        System.out.println(checkPalindrome(str));
+        System.out.println(checkPalindrome(str2));
+
+    }
+}
