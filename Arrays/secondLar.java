b82fc92847a97be4a9f2589600be24c0ebbece60
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-15 18:33:11 +0530

    Edge case added

--- Arrays/secondLar.java
+++ Arrays/secondLar.java
@@ -1,23 +1,33 @@
-import java.util.Scanner;
 
-public class secondLar {
-     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       int[] arr = new int[n];
-
-       for(int i=0;i<n;i++){
-        arr[i]=sc.nextInt();
-       }
-       int max = arr[0];
-       int secMax = arr[0];
-       for(int i=1;i<n;i++){
-        if(arr[i]>max){
-            secMax=max;
-            max=arr[i];
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
         }
-       }
-       System.out.println(secMax);
-       sc.close();
+        int largest =arr[0];
+        int sLargest =arr[0];
+        int tLargest = arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>largest){
+                tLargest = sLargest;
+                sLargest = largest;
+                largest = arr[i];
+            }
+            if(sLargest < arr[i] && arr[i]!= largest){
+                tLargest = sLargest;
+                sLargest = arr[i];
+            }
+             if(tLargest < arr[i] && arr[i]!= largest && arr[i]<sLargest){
+                tLargest = arr[i];
+            }
+            
+        }
+        System.out.print(tLargest+" "+largest+" "+sLargest);
+      sc.close();
     }
 }
