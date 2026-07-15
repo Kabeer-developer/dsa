6ba9903c53f4e03382db9f4a5ed63c61d28ecb0b
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-15 18:33:33 +0530

    rem dup

--- Arrays/remDuplicate.java
+++ Arrays/remDuplicate.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public class remDuplicate {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,4,5};
+        int[] arr = {1,5,6,4,6,2,9,7,4,3,8};
+        Arrays.sort(arr);
+
         for(int i=0;i<arr.length-1;i++){
             if(arr[i]!=arr[i+1]){
                 System.out.print(arr[i]+" ");
