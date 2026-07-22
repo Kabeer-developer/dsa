837a8b5df87a27dc0bbd3b6ed27c4c5714107896
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-22 19:31:28 +0530

    Sort char by freq

--- Strings/charByFreq.java
+++ Strings/charByFreq.java
@@ -1,22 +1,36 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class charByFreq {
     public static void main(String[] args) {
-       String str = "kabeer";
-       HashMap<Character,Integer> map = new HashMap<>();
-       
-       for(int i=0;i<str.length();i++){
-          char ch = str.charAt(i);
-          map.put(ch,map.getOrDefault(ch,0)+1);
-       }
-       
-       for(int freq=1;freq<=str.length();freq++){
-           for(int i=0;i<str.length();i++){
-               char ch = str.charAt(i);
-               if(map.get(ch)==freq){
-                   System.out.print(ch+":"+map.get(ch)+" ");
-               }
-           }
-       }
+        String str = "kabeer";
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+        }
+
+        for (int j = str.length(); j >= 0; j--) {
+            HashSet<Character> set = new HashSet<>();
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                if (!set.contains(ch)) {
+                    set.add(ch);
+                } else {
+                    continue;
+                }
+
+                if (map.get(ch) == j) {
+                    for (int l = 0; l < map.get(ch); l++) {
+                        result = result + ch;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+
     }
 }
