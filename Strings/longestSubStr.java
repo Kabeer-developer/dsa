1a91f54a888803d8a7ed29a995b2405b3b379bf9
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-29 22:43:05 +0530

    str printed

--- Strings/longestSubStr.java
+++ Strings/longestSubStr.java
@@ -4,7 +4,9 @@ public class longestSubStr {
     public static void main(String[] args) {
         int count =0;
         int max =0;
-        
+        int indStart =0;
+        int startIndex = 0;
+        int endIndex = 0;
         String str = "dvdf";
         for(int j=0;j<1;j++){
         HashMap<Character,Integer> map = new HashMap<>();         
@@ -13,11 +15,14 @@ public class longestSubStr {
             if(!map.containsKey(str.charAt(i))){
                 map.put(str.charAt(i),i);
                 count++;
+                startIndex = indStart;
+                endIndex = i;
             } else {
                 start = map.get(str.charAt(i));
                 count =0;
                 map.clear();
                 i=start;
+                indStart = i+1;
                 
             }
 
@@ -25,6 +30,9 @@ public class longestSubStr {
     
         }
         System.out.println(max);
+         for(int i=indStart;i<=endIndex;i++){
+                System.out.println(str.charAt(i));
+            }
     }
     
     }
