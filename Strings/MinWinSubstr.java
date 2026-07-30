df54b35804bd1200ac2ca7b1f294a034574e8717
Kabeer-developer <kabeeratwork7@gmail.com>
2026-07-30 21:06:32 +0530

    Min window substr

--- Strings/MinWinSubstr.java
+++ Strings/MinWinSubstr.java
@@ -1,31 +1,53 @@
 public class MinWinSubstr {
-    public static void main(String[] args) {
-        String str = "aab";
-        String target = "aab";
-        int size =target.length();
-        for(int i=0;i<str.length();i++){
-            for(int j=0;j<=str.length()-size;j++){
-                String temp = "";
-                for(int k=j;k<j+size;k++){
-                    temp+= str.charAt(k);
+    public static String minWinString(String s ,String t){
+        if(s==null || t==null || s.length()<t.length()){
+            return "";
+        }
+        int[] freq = new int[128];
+        for(char c : t.toCharArray()){
+            freq[c]++;
+        }
+
+        int left=0;
+        int right=0;
+        int count=t.length();
+        int start =0;
+        int minLength = Integer.MAX_VALUE;
+
+        while(right<s.length()){
+            char ch = s.charAt(right);
+            if(freq[ch]>0){
+                count--;
+            }
+            freq[ch]--;
+            right++;
+
+            while(count==0){
+                if(right-left < minLength){
+                    minLength = right-left;
+                    start = left;
                 }
-                int count =0;
-                for(int k=0;k<target.length();k++){
-                    
-                    for(int l=0;l<temp.length();l++){
-                        if(target.charAt(k) == temp.charAt(l)){
-                            count++;
-                        }
-                    }
-                    if(count == target.length()){
-                        System.out.println(temp);
-                        return;
-                    }
+
+                char leftChar = s.charAt(left);
+                freq[leftChar]++;
+                if(freq[leftChar]>0){
+                    count++;
                 }
                 
+                left++;
             }
-            if(size==str.length())return;
-            size++;
         }
+
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(start,start+minLength);
+
+    }
+    public static void main(String[] args) {
+        String s1 = "aarybaya";
+        String s2 = "aab";
+        String s3 = "abetdcabc";
+        String s4 = "abc";
+
+        System.out.println(minWinString(s1,s2));
+        System.out.println(minWinString(s3, s4));
     }
 }
