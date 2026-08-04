57e0a94faa71aec9a3e2ad0a1947b8629c291682
Kabeer-developer <kabeeratwork7@gmail.com>
2026-08-04 18:14:06 +0530

    delete from end

--- LinkedList/DeleteFromEnd.java
+++ LinkedList/DeleteFromEnd.java
@@ -8,24 +8,34 @@ class Node {
     }
 }
 public class DeleteFromEnd {
-    public static Node delElementByPosition(Node head,int position){
-        if(head == null){
-            return null;
-        }
+    public static Node deleteFromEnd(Node head, int position) {
+    if (head == null) return null;
 
-        if(position==0){
-            return head.next;
-        }
-        Node temp = head;
+    Node fast = head;
+    Node slow = head;
 
-        for(int i=0;i<position-1 && temp.next!=null;i++){
-            temp = temp.next;
+    // Move fast position + 1 steps
+    for (int i = 0; i <= position; i++) {
+        if (fast == null) {
+            return head; // Position is greater than list length
         }
-        if(temp.next != null){
-            temp.next = temp.next.next;
-        }
-        return head;
+        fast = fast.next;
     }
+
+    // Delete head if needed
+    if (fast == null) {
+        return head.next;
+    }
+
+    while (fast.next != null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+
+    return head;
+}
 public static void printList(Node head){
         Node temp = head;
         while(temp!=null){
@@ -40,7 +50,7 @@ public static void printList(Node head){
         head.next.next.next = new Node(40);
         head.next.next.next.next = new Node(50);
 
-        Node rHead = delElementByPosition(head, 0);
+        Node rHead = deleteFromEnd(head, 0);
         printList(rHead);
     }
 }
