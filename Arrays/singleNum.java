67b4458264f80981f5bb660a8808c53cecf183c4
Kabeer-developer <kabeeratwork7@gmail.com>
2026-08-05 17:49:54 +0530

    err resolved

--- Arrays/singleNum.java
+++ Arrays/singleNum.java
@@ -1,14 +1,23 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class singleNum {
     public static void main(String[] args) {
-        int[] arr = {1,7,7,8,9,9,2,3,4,4,5,5};
-       Arrays.sort(arr);;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i] != arr[i+1]){
-                System.out.print(arr[i]+ " ");
+        int[] arr = {1,7,7,7,8,9,9,2,3,4,4,5,5};
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicate = new HashSet<>();
+
+        for(int i=0;i<arr.length;i++){
+            if(!seen.contains(arr[i])){
+                seen.add(arr[i]);
             } else {
-                i++;
+                duplicate.add(arr[i]);
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(!duplicate.contains(arr[i])){
+                System.out.println(arr[i]);
             }
         }
     }
