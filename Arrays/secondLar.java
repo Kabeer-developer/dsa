cf414287e2f99f7efcfcc309b4a082dca2992757
Kabeer-developer <kabeeratwork7@gmail.com>
2026-08-05 17:50:22 +0530

    third largest added

--- Arrays/secondLar.java
+++ Arrays/secondLar.java
@@ -1,6 +1,6 @@
 
 import java.util.*;
-class Main {
+class secondLar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -9,25 +9,29 @@ class Main {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        int largest =arr[0];
-        int sLargest =arr[0];
-        int tLargest = arr[0];
-        for(int i=0;i<n;i++){
+        int largest =Integer.MIN_VALUE;
+        int sLargest =Integer.MIN_VALUE;
+        int tLargest = Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
             if(arr[i]>largest){
                 tLargest = sLargest;
                 sLargest = largest;
                 largest = arr[i];
+            } 
+
+            if(arr[i] != largest && arr[i] > sLargest){
+                tLargest=sLargest;
+                sLargest=arr[i];
             }
-            if(sLargest < arr[i] && arr[i]!= largest){
-                tLargest = sLargest;
-                sLargest = arr[i];
-            }
-             if(tLargest < arr[i] && arr[i]!= largest && arr[i]<sLargest){
+
+            if(arr[i] > tLargest && arr[i]!=sLargest && arr[i]!= largest){
                 tLargest = arr[i];
             }
-            
+
         }
-        System.out.print(tLargest+" "+largest+" "+sLargest);
+
+        System.out.println(largest+" "+sLargest+" "+tLargest);
       sc.close();
     }
 }
